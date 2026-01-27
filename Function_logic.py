79391f93d98e7d23e3826fe8fc2eb141ce32b8d2
Christian <chrissy.maschke@web.de>
2026-01-27 12:11:09 +0100

    [Cm |27.01.2026 | Create first Funktionnames]

--- Function_logic.py
+++ Function_logic.py
@@ -7,21 +7,23 @@ import UI
 
 
 
-def generate_board(board: list):
+def generate_board():
 
-    board_zeile = ["","","","","","",""]
+    board_zeile = ["","a","","","","",""]
     board = []
     counter = 1
 
-    while counter <= 7:
+    while counter <= 6:
         board.append(board_zeile)
-        counter = counter + 1    
-    return
+        counter = counter + 1   
+    return board
 
 
 
 def check_horizontal(board,player):
-    winner = 1
+    for zeile in board:
+        
+        winner = 1
     return winner
 
 def check_vertical(board,player):
@@ -31,3 +33,20 @@ def check_vertical(board,player):
 def check_diagonal(boarrd,player):
     winner = 1
     return winner
+
+board = generate_board()
+aktuelles_symbol = 
+for zeile in board:
+    for kaestchen in zeile:
+        if kaestchen != "":
+            aktuelles_symbol = kaestchen
+
+           
+        else:
+            print("besetzt!")
+
+            
+            
+
+print("")
+    
