4b560671bd2b821ed301fd4e09eb73f940821c39
Christian <chrissy.maschke@web.de>
2026-01-27 14:07:38 +0100

    [Cm|27.01.2026|Horizontal io, vertical teilweise]

--- Function_logic.py
+++ Function_logic.py
@@ -13,12 +13,55 @@ def generate_board():
     board = []
     counter = 1
 
-    while counter <= 6:
+    while counter <= 7:
         board.append(board_zeile)
-        counter = counter + 1   
+        counter = counter + 1    
     return board
 
 
+    # ["","x","x","","","",""]
+    # ["","x","x","","","",""]
+    # ["","x","x","","","",""]
+    # ["","","x","","","",""]
+    # ["","","","","","",""]
+    # ["","","","","","",""]
+
+
+#Überprüfung vertikal
+board = [ ["","","","","","",""], ["","","","","","",""], ["","","","","","",""], ["","","","","","",""], ["","","","","","",""], ["","","","","","",""]]
+counter_horizontal = 0
+counter_vertical = 0
+counter = 1
+gewonnen = 0
+
+for zeile in board:
+    for kaestchen in zeile:
+        if kaestchen != "":
+            kaestchen_symbol = kaestchen
+
+            if kaestchen_symbol == board[counter_horizontal + 1][counter_vertical ]:
+                counter = counter + 1
+            
+                if kaestchen_symbol == board[counter_horizontal + 2][counter_vertical ]:
+                    counter = counter + 1
+
+                    if kaestchen_symbol == board[counter_horizontal + 3][counter_vertical ]:
+                        counter = counter + 1
+
+        if counter >= 4:
+            gewonnen = 1
+            break
+            
+                
+        counter_vertical = counter_vertical + 1
+
+    counter_vertical = 0
+    counter_horizontal = counter_horizontal + 1
+
+print(gewonnen)
+            
+
+            
 
 def check_horizontal(board,player):
     for zeile in board:
@@ -59,22 +102,25 @@ def check_diagonal(boarrd,player):
 
 # print(gewonnen)
 
-board = generate_board()
-gewonnen = 0
-for zeile in range(0,len(board)-1):
-    for kaestchen in range(1,len(board[zeile])-1):
-        board[kaestchen][zeile]
-        if board[kaestchen][zeile] == board[kaestchen-1][zeile]:
-            counter = counter + 1
+
+# Test vertikal Olli
+
+# board = generate_board()
+# gewonnen = 0
+# for zeile in range(0,len(board)-1):
+#     for kaestchen in range(1,len(board[zeile])-1):
+#         board[kaestchen][zeile]
+#         if board[kaestchen][zeile] == board[kaestchen-1][zeile]:
+#             counter = counter + 1
         
-        else : 
-            counter = 1
+#         else : 
+#             counter = 1
 
-        if counter >= 4:
-            gewonnen = 1
-            break
+#         if counter >= 4:
+#             gewonnen = 1
+#             break
 
-print(gewonnen)
+# print(gewonnen)
 
         
         
